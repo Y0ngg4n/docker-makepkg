4b90816afb68cc82b813b514e82fb7f90c0cf620
Yonggan <30302975+Y0ngg4n@users.noreply.github.com>
2021-03-20 15:24:50 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -33,8 +33,11 @@ NEXUSPKGNAME="$PKGNAME-$PKGVERSION-$PKKARCH.pkg.tar.zst"
 
 curl -sSL -k -X GET -G "$REPO_API_URL/assets?repository=oblivion-os" > output
 cat output | jq '.items | map(.path)' > jq-output
+echo "Packages existing:"
+cat jq-output
 for k in $(jq -r ".[]" jq-output); do 
     if [ "$k" == "$NEXUSPKGNAME" ]
+        echo "Package allready exists"
         then exit 0
     fi
 done
